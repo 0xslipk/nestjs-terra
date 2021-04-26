eef2c3a3d9cff7e04d2c21aeb038c97175b58896
Jose Ramirez <jarcodallo@gmail.com>
2021-04-26 17:17:51 -0300

    typo

--- src/terra.constants.ts
+++ src/terra.constants.ts
@@ -3,6 +3,6 @@ export const TERRA_MODULE_OPTIONS = 'TerraModuleOptions';
 export const DEFAULT_GAS_PRICE = '0.00506uluna';
 export const DEFAULT_GAS_ADJUSTMENT = 1.4;
 export const TERRA_LCD_BASE_URL = 'https://lcd.terra.dev';
-export const TERRA_LCD_TESTNET_BASE_URL = 'https://tequila-lcd.terra.dev';
+export const TERRA_TESTNET_LCD_BASE_URL = 'https://tequila-lcd.terra.dev';
 export const TERRA_TESTNET_CHAIN_ID = 'tequila-0004';
 export const TERRA_MAINNET_CHAIN_ID = 'columbus-4';
