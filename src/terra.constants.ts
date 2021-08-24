ccd23139f550ff97cf1bfbbf3708d2301e193d33
Jose Ramirez <jarcodallo@gmail.com>
2021-08-24 19:46:07 -0300

    decorator name

--- src/terra.constants.ts
+++ src/terra.constants.ts
@@ -1,4 +1,4 @@
-export const DECORATED_PREFIX = 'TerraJS';
+export const DECORATED_PREFIX = 'TerraJS:V2';
 export const TERRA_MODULE_OPTIONS = 'TerraModuleOptions';
 export const DEFAULT_GAS_PRICE = '0.00506uluna';
 export const DEFAULT_GAS_ADJUSTMENT = 1.4;
